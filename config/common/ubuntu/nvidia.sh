7bb9f89b2bd618dcae5b102546fce0610e5fe036
Ubuntu <ubuntu@titanosaurus.novalocal>
2019-05-15 19:20:50 +0000

    flexible debfile for nvidia

--- config/common/ubuntu/nvidia.sh
+++ config/common/ubuntu/nvidia.sh
@@ -1,9 +1,12 @@
 # install system dependencies
 sudo apt-get install -y linux-headers-$(uname -r)
 
+# deb
+deb=cuda-repo-ubuntu1804_10.1.105-1_amd64.deb
+
 # install nvidia driver and  cuda toolkit
-wget -N https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.105-1_amd64.deb && \
-        sudo dpkg -i cuda-repo-ubuntu1804_10.1.105-1_amd64.deb
+wget  https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/$deb && sudo dpkg -i $deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo apt-get update
 sudo apt-get install -y cuda
+rm $deb
