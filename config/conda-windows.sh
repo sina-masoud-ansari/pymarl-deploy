26be1eecc02451f750999e35ca62f744809ce72d
Sina Masoud-Ansari <sina.masoud.ansari@gmail.com>
2019-06-18 19:27:49 +1200

    path check for SC2 on windows

--- config/conda-windows.sh
+++ config/conda-windows.sh
@@ -6,11 +6,18 @@ url=$1 # pymarl repo url
 dest=$2 # destination dir
 
 git clone $url $dest
-source $(dirname $(which $CONDA_EXE))/../etc/profile.d/conda.sh
+source $(dirname $(which "${CONDA_EXE}"))/../etc/profile.d/conda.sh
 conda create -y -n pymarl python=3.5
 conda activate pymarl
 conda install -y pytorch=0.4.1 cuda90 -c pytorch
-pip install --upgrade pip
+python -m pip install --upgrade pip
 pip install -r $dest/requirements.txt
-echo "Please see $2/install_sc2.sh and https://github.com/oxwhirl/smac#installing-starcraft-ii for details on installing StarCraft II and the required maps"
 
+# check SC2PATH env var
+echo ""
+if [ -z "${SC2PATH}" ]; then
+    echo "Warning: SC2PATH doesn't appear to be set "
+    echo "Please see $2/install_sc2.sh and https://github.com/oxwhirl/smac#installing-starcraft-ii for details on installing StarCraft II and the required maps"
+else
+    echo "SC2PATH=${SC2PATH}"
+fi
